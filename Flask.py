40764a1193bfa14c20b9c50bd6e2c2018ab8224e
aawad93 <andrew.awad93@gmail.com>
2019-08-07 11:16:27 -0700

    This stuff is just weiiird

--- Flask.py
+++ Flask.py
@@ -1,15 +1,15 @@
 import sqlalchemy
+import pandas as pd
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-
+import datetime as dt
 from flask import Flask, jsonify
 
 
 
 # Database Setup
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
-conn = engine.connect()
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -36,35 +36,35 @@ def welcome():
     return (
         f"<h1> Welcome to Andrew's Homework </h1>"
         f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/percipitation<br/>"
         f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs"
-        f"/api/v1.0/<start>"
-        f"/api/v1.0/<end>"
-        f"/api/v1.0/<start>/<end>"
+        f"/api/v1.0/tobs<br/>"
+        f"<h2> dates should be entered YYYY-MM-DD <br/></h2>"
+        f"/api/v1.0/start=<date><br/>"
+        f"      ex: /api/v1.0/start=2015-08-07<br/>"
+        f"/api/v1.0/end=<date><br/>"
+        f"      ex: /api/v1.0/end=2015-08-07<br/>"
+        f"/api/v1.0/start=<date>/end=<date><br/>"
+        f"      ex: /api/v1.0/start=2015-08-07/end=2016-08-07<br/>"
+        
     )
 
 @app.route("/api/v1.0/percipitation")
 def percipitation():
     """the last 12 months of precipitation"""
-    # Query percipitation data
-    session = Session(engine)
-    prcp = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= '2016-08-18')
-    
-  # Create a dictionary from the row data and append to a list
-    prcp_data = []
-    for date, p in prcp:
-        prcp_dict = {}
-        prcp_dict["date"] = date
-        prcp_dict["prcp"] = p
-        prcp_data.append(prcp_dict)
+   # Query 
+    conn = engine.connect()
+    prcp_df = pd.read_sql("SELECT date, prcp FROM measurement WHERE date BETWEEN '2016-08-23' AND '2017-08-23'", conn).set_index('date').sort_index(axis=0)
 
-    return jsonify(prcp_data)
+    prcp_html= prcp_df.to_html()
+
+    return (prcp_html)
 
 @app.route("/api/v1.0/stations")
 def stations():
     """all stations"""
     # Query all stations
+    session = Session(engine)
     stations = session.query(Station.station).all()
 
     return jsonify(stations)
@@ -86,30 +86,32 @@ def tobs():
 
     return jsonify(tobs_data)
 
-@app.route("/api/v1.0/<start>")
-def start_date_query(start):
+@app.route("/api/v1.0/start=<start_date>")
+def start_date_query(start_date):
     """Fetch the temperature data from start date"""
-    start_date = start.replace("/","-").replace(" ","-")
-    tobs_start = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).filter(Measurement.date >= start_date)
+    conn = engine.connect()
+    tobs_start_df = pd.read_sql_query(f"SELECT MIN(tobs), MAX(tobs), AVG(tobs) FROM measurement WHERE date >={start_date}", conn)
+    tobs_start_html = tobs_start_df.to_html()
 
-    return jsonify(tobs_start)
+    return (tobs_start_html)
 
-@app.route("/api/v1.0/<end>")
-def start_date_query(end):
-    """Fetch the temperature data from start date"""
-    end_date = end.replace("/","-").replace(" ","-")
-    tobs_end = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).filter(Measurement.date <= end_date)
+@app.route("/api/v1.0/end=<end_date>")
+def end_date_query(end_date):
+    """Fetch the temperature data from end date"""
+    conn = engine.connect()
+    tobs_end_df = pd.read_sql_query(f"SELECT MIN(tobs), MAX(tobs), AVG(tobs) FROM measurement WHERE date <={end_date}", conn)
+    tobs_end_html = tobs_end_df.to_html()
 
-    return jsonify(tobs_end)
+    return (tobs_end_html)
 
-@app.route("/api/v1.0/<start>/<end>")
-def start_date_query(start, end):
-    """Fetch the temperature data from start date"""
-    start_date = start.replace("/","-").replace(" ","-")
-    end_date = end.replace("/","-").replace(" ","-")
-    tobs_custom = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).filter(Measurement.date >= start_date).filter(Measurement.date <= end_date)
+@app.route("/api/v1.0/start=<start_date>/end=<end_date>")
+def start_end_query(start_date, end_date):
+    """Fetch the temperature data from start and end date"""
+    conn = engine.connect()
+    tobs_cust_df = pd.read_sql_query(f"SELECT MIN(tobs), MAX(tobs), AVG(tobs) FROM measurement WHERE date BETWEEN {start_date} AND {end_date}", conn)
+    tobs_cust_html = tobs_cust_df.to_html()
 
-    return jsonify(tobs_custom)
+    return (tobs_cust_html)
 
 
 if __name__ == '__main__':
